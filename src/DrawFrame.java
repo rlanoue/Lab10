1866ee90bfcb89eb15516414967bb5e8b7f268c2
Jordan Lanoue <rlanoue@ou.edu>
2019-04-11 23:09:20 -0500

    Added the animated commands that I dabbled with off of the lab10 demo

--- src/DrawFrame.java
+++ src/DrawFrame.java
@@ -1,6 +1,47 @@
-
+import javax.swing.JFrame;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import javax.swing.JPanel; 
+/**
+ * 
+ * @author Jordan Lanoue
+ *
+ */
 public class DrawFrame extends JFrame {
 
+	//instance object of the drawpanel class
 	private DrawPanel drawPanel; 
+
+	public DrawFrame(String title) {
+
+	}
 	
+	//private static final long serialVersionUID = 1L;
+			static int x = 0; 
+			static int y = 0; 
+	private void moveText() {
+		x = x + 1; 
+		y = y + 1; 
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		
+
+		
+		JFrame frame = new JFrame("Graphics Demo");
+		DrawFrame animation = new DrawFrame("Animated!"); 
+		frame.add(animation); 
+		frame.setSize(400, 400);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+		while(true) 
+		{
+			animation.moveText(); 
+			animation.repaint();  
+			Thread.sleep(10); 
+		}
+	}
 }
--- src/DrawPanel.java
+++ src/DrawPanel.java
@@ -1,4 +1,16 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 
 public class DrawPanel {
 
+	
+	@Override
+	protected void paintComponent(Graphics g)
+	{
+		super.paintComponent((java.awt.Graphics) g);
+		Graphics2D g2d = (Graphics2D) g; 
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		g2d.drawString("Moo", x, y);//(x, y, 30, 30);
+	}
 }
